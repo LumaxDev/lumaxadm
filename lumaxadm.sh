6a5a0edd012d20d2dbb662317fa726ac405bc1c5
macos <lumaxdev.project@gmail.com>
2026-04-15 13:49:25 +0600

    refresh version

--- lumaxadm.sh
+++ lumaxadm.sh
@@ -22,7 +22,7 @@ while [ -h "$SOURCE" ]; do
 done
 export SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-readonly VERSION="v4.0"
+readonly VERSION="v1.0"
 
 # ============================================================ #
 #              ПОДГОТОВКА И ЗАГРУЗКА КОМПОНЕНТОВ               #
